233b5cbffdf3b482c01f576f7bdadf77a48159d9
wyldphyre <craig@wyldphyre.com>
2014-06-18 19:50:48 +0800

    Initial commit
    No real functionality at this stage, just the stub of a system tray
    application.

---
+++ HQLinkerApplicationContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQLinker
+{
+  class HQLinkerApplicationContext : SystemTrayApplicationContext
+  {
+    public HQLinkerApplicationContext() : base()
+    {
+      base.IconDoubleClickEvent += HQLinkerApplicationContext_IconDoubleClickEvent;
+      base.ContextMenuOpeningEvent += HQLinkerApplicationContext_ContextMenuOpeningEvent;
+
+      Initialise();
+    }
+
+    private void Initialise()
+    {
+      AddMenuItem("Exit", null, ExitMenuItemClick);
+
+      StartMonitoring();
+    }
+
+    private void ExitMenuItemClick(object sender, EventArgs e)
+    {
+      ExitApplication();
+    }
+
+    private void ToggleMonitoring()
+    {
+      if (Monitoring)
+        StopMonitoring();
+      else
+        StartMonitoring();
+    }
+    private void StartMonitoring()
+    {
+      Monitoring = true;
+      SetIconImage(HQLinker.Properties.Resources.Link16x16);
+    }
+    private void StopMonitoring()
+    {
+      Monitoring = false;
+      SetIconImage(HQLinker.Properties.Resources.LinkBroken16x16);
+    }
+
+    private void HQLinkerApplicationContext_ContextMenuOpeningEvent(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+    }
+    private void HQLinkerApplicationContext_IconDoubleClickEvent()
+    {
+      ToggleMonitoring();
+    }
+
+    private bool Monitoring;
+  }
+}
