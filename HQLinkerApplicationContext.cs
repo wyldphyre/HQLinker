2bff9ff3548135983324a2436e6faebe63634990
wyldphyre <craig@wyldphyre.com>
2014-06-18 21:38:30 +0800

    Initial functional version
    The app now does the bare minimum of monitoring the clipboard for hq://
    links put on the clipboard by apps other than HQ and then opening the
    links.
    
    There is still plenty of work to do in order to clean up the code and
    extend the functionality.

--- HQLinkerApplicationContext.cs
+++ HQLinkerApplicationContext.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Interop;
 
 namespace HQLinker
 {
   class HQLinkerApplicationContext : SystemTrayApplicationContext
   {
+    [DllImport("user32.dll")] static extern IntPtr GetClipboardOwner();
+
     public HQLinkerApplicationContext() : base()
     {
       base.IconDoubleClickEvent += HQLinkerApplicationContext_IconDoubleClickEvent;
@@ -25,6 +31,7 @@ namespace HQLinker
 
     private void ExitMenuItemClick(object sender, EventArgs e)
     {
+      StopMonitoring();
       ExitApplication();
     }
 
@@ -39,9 +46,44 @@ namespace HQLinker
     {
       Monitoring = true;
       SetIconImage(HQLinker.Properties.Resources.Link16x16);
+
+      this.ClipboardNotification = new ClipboardNotification();
+      ClipboardNotification.ClipboardUpdate += ClipboardNotification_ClipboardUpdate;
+    }
+
+    void ClipboardNotification_ClipboardUpdate(object sender, EventArgs e)
+    {
+      if (Clipboard.ContainsText())
+      {
+        var Text = Clipboard.GetText();
+        if (Text != LastText)
+        {
+          IntPtr OwnerWindowHandle = GetClipboardOwner();
+          var AppName = "";
+
+          if (!Text.StartsWith("hq://"))
+            return;
+
+          if (OwnerWindowHandle != IntPtr.Zero)
+          {
+            unsafe
+            {
+              var ProcessId = win32.GetWindowProcessID((int)OwnerWindowHandle.ToPointer());
+              AppName = Process.GetProcessById(ProcessId).ProcessName;
+            }
+          }
+
+          if (AppName != "HQClient")
+            System.Diagnostics.Process.Start(Text);
+          
+          LastText = Text;
+        }
+      }
     }
     private void StopMonitoring()
     {
+      this.ClipboardNotification = null;
+
       Monitoring = false;
       SetIconImage(HQLinker.Properties.Resources.LinkBroken16x16);
     }
@@ -55,5 +97,92 @@ namespace HQLinker
     }
 
     private bool Monitoring;
+    private ClipboardNotification ClipboardNotification;
+    private string LastText;
+  }
+
+  /// <summary>
+  /// Provides notifications when the contents of the clipboard is updated.
+  /// </summary>
+  public sealed class ClipboardNotification
+  {
+    /// <summary>
+    /// Occurs when the contents of the clipboard is updated.
+    /// </summary>
+    public static event EventHandler ClipboardUpdate;
+
+    private static NotificationForm _form = new NotificationForm();
+
+    /// <summary>
+    /// Raises the <see cref="ClipboardUpdate"/> event.
+    /// </summary>
+    /// <param name="e">Event arguments for the event.</param>
+    private static void OnClipboardUpdate(EventArgs e)
+    {
+      var handler = ClipboardUpdate;
+      if (handler != null)
+      {
+        handler(null, e);
+      }
+    }
+
+    /// <summary>
+    /// Hidden form to recieve the WM_CLIPBOARDUPDATE message.
+    /// </summary>
+    private class NotificationForm : Form
+    {
+      public NotificationForm()
+      {
+        NativeMethods.SetParent(Handle, NativeMethods.HWND_MESSAGE);
+        NativeMethods.AddClipboardFormatListener(Handle);
+      }
+
+      protected override void WndProc(ref Message m)
+      {
+        if (m.Msg == NativeMethods.WM_CLIPBOARDUPDATE)
+        {
+            OnClipboardUpdate(null);
+        }
+        base.WndProc(ref m);
+      }
+    }
+  }
+
+  internal static class NativeMethods
+  {
+    // See http://msdn.microsoft.com/en-us/library/ms649021%28v=vs.85%29.aspx
+    public const int WM_CLIPBOARDUPDATE = 0x031D;
+    public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+
+    // See http://msdn.microsoft.com/en-us/library/ms632599%28VS.85%29.aspx#message_only
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+    // See http://msdn.microsoft.com/en-us/library/ms633541%28v=vs.85%29.aspx
+    // See http://msdn.microsoft.com/en-us/library/ms649033%28VS.85%29.aspx
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+  }
+
+  class win32
+  {
+    [DllImport("user32")]
+    private static extern UInt32 GetWindowThreadProcessId(
+      Int32 hWnd,
+      out Int32 lpdwProcessId
+    );
+
+    public static Int32 GetWindowProcessID(Int32 hwnd)
+    {
+      Int32 pid = 1;
+      GetWindowThreadProcessId(hwnd, out pid);
+      return pid;
+    }
+
+    public win32()
+    {
+
+    }
   }
 }
